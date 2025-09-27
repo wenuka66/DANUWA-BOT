4571b0fc8984e836985177bb8bc7e77d60ecca55
wenuka66 <gemingsudda66@gmail.com>
2025-09-27 13:45:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ function convertToBool(text, fault = 'true') {
 
 
 const defaultConfig = {
-  SESSION_ID: "", // Put your session id here
+  SESSION_ID: "fYYl1YiA#lWHRH_-9JVIdCctCu0rIln2lXhizYpQ5D20BdNfZAe0", // Put your session id here
   ALIVE_IMG: "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
-  ALIVE_MSG: "*Hello👋 DANUWA-MD Is Alive Now😍*", // Change alive msg from here
+  ALIVE_MSG: "*Hello👋 RASTHIYADU-MD Is Alive Now😍*", // Change alive msg from here
   OPENAI_API_KEY: "",// Add OPENAI API key here
   GEMINI_API_KEY: "", // Add Gemini API key here
   REMOVE_BG_API_KEY:"", // Add removebg API key here
